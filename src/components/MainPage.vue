edceacac22947222fec3f290a855f4ad2d2bae8e
awaae001 <3271436144@qq.com>
2025-02-26 18:53:36 +0800

    feat(components): 添加欢迎页面并调整页面逻辑
    - 在 MainPage 组件中添加 WelcomePage 组件
    - 修改 currentPage 类型，增加 'welcome' 页面
    - 调整 CharacterCardEditor 和 WorldEditor 组件的按钮布局

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="main-container">
+    <WelcomePage v-if="currentPage === 'welcome'" />
     <WorldEditor v-if="currentPage === 'world'" ref="worldEditorRef" />
     <CharacterCardEditor v-if="currentPage === 'character'" ref="characterEditorRef" />
   </div>
@@ -9,12 +10,13 @@
 import { ref } from 'vue'
 import WorldEditor from './WorldEditor.vue'
 import CharacterCardEditor from './CharacterCardEditor.vue'
+import WelcomePage from './WelcomePage.vue'
 
-const currentPage = ref<'world' | 'character'>('character')
+const currentPage = ref<'world' | 'character' | 'welcome'>('welcome')
 const characterEditorRef = ref<InstanceType<typeof CharacterCardEditor>>()
 const worldEditorRef = ref<InstanceType<typeof WorldEditor>>()
 
-function showPage(page: 'world' | 'character') {
+function showPage(page: 'world' | 'character' | 'welcome') {
   currentPage.value = page
 }
 
