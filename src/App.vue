06ddea1d633ccd65d9eb7e81663e3f0e82e4847a
awaae001 <3271436144@qq.com>
2025-03-06 23:35:54 +0800

    feat(index): 添加 Umami 统计并更新版本号
    - 在 index.html 中添加 Umami 统计脚本
    - 更新 package.json 中的版本号至 0.1.3
    - 在 App.vue 中添加 GitHub 链接按钮
    - 调整版本号显示位置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { Icon } from '@iconify/vue'
+
+const openGithub = () => {
+  window.open('https://github.com/awaae001/st_cardplus', '_blank')
+}
 </script>
 
 <template>
@@ -11,9 +16,14 @@ import { RouterView } from 'vue-router'
         <div class="menu-item" @click="$router.push('/world')">地标编辑器</div>
         <div class="menu-item" @click="$router.push('/cardoutput')">角色卡快搭</div>
       </div>
-      <p class="whatYouwant">dev_0.1.2</p>
-      <!-- <div class="menu-item" @click="handleSave">保存</div>
+      <div style="display: flex; align-items: center;">
+        <el-button type="primary" circle @click="openGithub" style="margin-right: 6px;">
+          <Icon icon="devicon:github" width="16" height="16" />
+        </el-button>
+        <p class="whatYouwant">dev_0.1.3</p>
+        <!-- <div class="menu-item" @click="handleSave">保存</div>
       <div class="menu-item" @click="handleLoad">加载</div> -->
+      </div>
     </div>
     <slot />
   </div>
